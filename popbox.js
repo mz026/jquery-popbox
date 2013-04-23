398b275d7e076f602d4ed138d83c96c382689940
Yang-Hsing Lin <yanghsing.lin@thekono.com>
2013-04-24 02:39:54 +0800

    fix multiple declare on the same box

--- popbox.js
+++ popbox.js
@@ -1,119 +1,122 @@
 (function(){
 
   $.fn.popbox = function(options){
-    var settings = $.extend({
-      selector      : this.selector,
-      open          : '.open',
-      box           : '.box',
-      arrow         : '.arrow',
-      arrow_border  : '.arrow-border',
-      close         : '.close',
-      // if popbox float when resizing window, will alter container width
-      dynamic_width: false,
-      placement: 'bottom'
-    }, options);
-
-    var box = $(this).find(settings['box']);
-    var arrowStyle, arrowBorderStyle;
-    var element = $(this);
-
-    var methods = {
-      open: function(event){
-        event.preventDefault();
-
-        var pop = $(this), topOffset;
-        if ( settings.placement === 'top' ) {
-          topOffset = box.height() * (-1) - $(this).height() - 8;
-        } else {
-          topOffset = 10;
-        }
-
-        box.find(settings['arrow']).css({'left': box.width()/2 - 10});
-        box.find(settings['arrow_border']).css({'left': box.width()/2 - 10});
-
-        if(box.css('display') == 'block'){
-          methods.close();
-        } else {
-          box.css({
-            'display': 'block', 
-            'top': topOffset,
-            'left': ((pop.parent().width() * 0.5) - box.width() * 0.5 )
-          });
-          setupArrow();
-        }
-      },
-
-      close: function(){
-        box.fadeOut("fast");
-      }
-    };
-
-    $(document).bind('keyup', function(event){
-      if(event.keyCode == 27){
-        methods.close();
-      }
-    });
-
-    $(document).bind('click', function(event){
-      if(!$(event.target).closest(settings['selector']).length){
-        methods.close();
-      }
-    });
-
-    // setup css
-    $(this).find(".collapse").css({
-      position: "relative" 
-    });
-
-    $(this).find(settings["box"]).css({
-      display: "none",
-      position: "absolute" 
-    });
-
-    arrowStyle = {
-      "width": "0",
-      "height": "0",
-      "border-left": "11px solid transparent",
-      "border-right": "11px solid transparent",
-      "position": "absolute",
-      "z-index": "1001"
-    };
-    arrowBorderStyle = {
-      "width": "0",
-      "height": "0",
-      "border-left": "11px solid transparent",
-      "border-right": "11px solid transparent",
-      "position":"absolute",
-      "z-index":"1000"
-    };
-
-    function setupArrow () {
-      if ( settings.placement === "top" ) {
-        arrowStyle["border-top-style"] = "solid";
-        arrowStyle["border-top-width"] = "11px";
-        arrowStyle["top"] = box.height() - 1;
-
-        arrowBorderStyle["border-top-style"] = "solid";
-        arrowBorderStyle["border-top-width"] = "11px";
-        arrowBorderStyle["top"] = box.height() + 1;
-      } else {
-        arrowStyle["border-bottom-style"] = "solid";
-        arrowStyle["border-bottom-width"] = "11px";
-        arrowStyle["top"] = "-10px";
-
-        arrowBorderStyle["border-bottom-style"] = "solid";
-        arrowBorderStyle["border-bottom-width"] = "11px";
-        arrowBorderStyle["top"] = "-12px";
-      }
-      element.find(settings["arrow"]).css(arrowStyle);
-      element.find(settings["arrow_border"]).css(arrowBorderStyle);
-    }
-
-
-
-
+    var selector = this.selector;
 
     return this.each(function(){
+      var settings = $.extend({
+        selector      : selector,
+        open          : '.open',
+        box           : '.box',
+        arrow         : '.arrow',
+        arrow_border  : '.arrow-border',
+        close         : '.close',
+        // if popbox float when resizing window, will alter container width
+        dynamic_width: false,
+        placement: 'bottom'
+      }, options);
+
+      var box = $(this).find(settings['box']);
+      var arrowStyle, arrowBorderStyle;
+      var element = $(this);
+
+      var methods = {
+        open: function(event){
+          event.preventDefault();
+
+          var pop = $(this), topOffset;
+          if ( settings.placement === 'top' ) {
+            topOffset = box.height() * (-1) - $(this).height() - 8;
+          } else {
+            topOffset = 10;
+          }
+
+          box.find(settings['arrow']).css({'left': box.width()/2 - 10});
+          box.find(settings['arrow_border']).css({'left': box.width()/2 - 10});
+
+          if(box.css('display') == 'block'){
+            methods.close();
+          } else {
+            box.css({
+              'display': 'block', 
+              'top': topOffset,
+              'left': ((pop.parent().width() * 0.5) - box.width() * 0.5 )
+            });
+            setupArrow();
+          }
+        },
+
+        close: function(){
+          box.fadeOut("fast");
+        }
+      };
+
+      $(document).bind('keyup', function(event){
+        if(event.keyCode == 27){
+          methods.close();
+        }
+      });
+
+      $(document).bind('click', function(event){
+        //TODO fix this if no selector
+        if(!$(event.target).closest(settings['selector']).length){
+          methods.close();
+        }
+      });
+
+      // setup css
+      $(this).find(".collapse").css({
+        position: "relative" 
+      });
+
+      $(this).find(settings["box"]).css({
+        display: "none",
+        position: "absolute" 
+      });
+
+      arrowStyle = {
+        "width": "0",
+        "height": "0",
+        "border-left": "11px solid transparent",
+        "border-right": "11px solid transparent",
+        "position": "absolute",
+        "z-index": "1001"
+      };
+      arrowBorderStyle = {
+        "width": "0",
+        "height": "0",
+        "border-left": "11px solid transparent",
+        "border-right": "11px solid transparent",
+        "position":"absolute",
+        "z-index":"1000"
+      };
+
+      function setupArrow () {
+        if ( settings.placement === "top" ) {
+          arrowStyle["border-top-style"] = "solid";
+          arrowStyle["border-top-width"] = "11px";
+          arrowStyle["top"] = box.height() - 1;
+
+          arrowBorderStyle["border-top-style"] = "solid";
+          arrowBorderStyle["border-top-width"] = "11px";
+          arrowBorderStyle["top"] = box.height() + 1;
+        } else {
+          arrowStyle["border-bottom-style"] = "solid";
+          arrowStyle["border-bottom-width"] = "11px";
+          arrowStyle["top"] = "-10px";
+
+          arrowBorderStyle["border-bottom-style"] = "solid";
+          arrowBorderStyle["border-bottom-width"] = "11px";
+          arrowBorderStyle["top"] = "-12px";
+        }
+        element.find(settings["arrow"]).css(arrowStyle);
+        element.find(settings["arrow_border"]).css(arrowBorderStyle);
+      }
+
+
+
+
+
       if ( settings['dynamic_width'] ) {
         // Width needs to be set otherwise popbox will not move when window resized.
         $(this).css({'width': box.width()}); 
